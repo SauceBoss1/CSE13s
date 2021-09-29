06b0c46aa0da4f884e4865740ed92bc277f6dc1c
dtercia <dtercian@ucsc.edu>
2021-09-29 18:11:48 +0000

    updating pig.c

--- asgn1/pig.c
+++ asgn1/pig.c
@@ -1,9 +1,10 @@
+#include "names.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "names.h"
 
-typedef enum { SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER} Position;
+typedef enum { SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER } Position;
 const Position pig[7] = {
     SIDE, //0
     SIDE, //0
@@ -14,15 +15,14 @@ const Position pig[7] = {
     JOWLER, //4
 };
 
-
-int scores[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int scores[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 int score_checker(int cur_player);
 int score_checkAll(void);
 int points_updater(int cur_player);
 int game_master(int numPlayers);
 
-int score_checker(int cur_player){
+int score_checker(int cur_player) {
     if (scores[cur_player] >= 100) {
         printf("\n%s has won with %d!\n", names[cur_player], scores[cur_player]);
         return scores[cur_player];
@@ -30,47 +30,47 @@ int score_checker(int cur_player){
     return 0;
 }
 
-int points_updater(int cur_player){
+int points_updater(int cur_player) {
     printf("%s rolls the pig... ", names[cur_player]);
-    while (1){
+    while (1) {
         int pigPos = (random() % 7);
-        if (pig[pigPos] == 1){
+        if (pig[pigPos] == 1) {
             scores[cur_player] += 10;
             printf("pig lands on back ");
-        } else if (pig[pigPos] == 2){
-	    scores[cur_player] += 10;
+        } else if (pig[pigPos] == 2) {
+            scores[cur_player] += 10;
             printf("pig lands upright ");
-        } else if (pig[pigPos] == 3){
+        } else if (pig[pigPos] == 3) {
             scores[cur_player] += 15;
             printf("pig lands on snout ");
-        } else if(pig[pigPos] == 4){
+        } else if (pig[pigPos] == 4) {
             scores[cur_player] += 5;
             printf("pig lands on ear ");
-        } else{
+        } else {
             printf("pig lands on side\n");
             break;
         }
-        if (score_checker(cur_player) != 0){
+        if (score_checker(cur_player) != 0) {
             break;
-        } 
+        }
     }
     return 0;
 }
 
-int score_checkAll (void){
-    for(int i = 0; i <=10; i++){
-        if (scores[i] >= 100){
+int score_checkAll(void) {
+    for (int i = 0; i <= 10; i++) {
+        if (scores[i] >= 100) {
             return scores[i];
         }
     }
     return 0;
 }
 
-int game_master(int numPlayers){
+int game_master(int numPlayers) {
     int cur_player = 0;
-    while (score_checkAll() == 0){
+    while (score_checkAll() == 0) {
         points_updater(cur_player);
-        if (cur_player >= (numPlayers-1)){
+        if (cur_player >= (numPlayers - 1)) {
             cur_player = 0;
         } else {
             cur_player += 1;
@@ -85,7 +85,7 @@ int game_master(int numPlayers){
     return 0;
 }
 
-int main (void){ // main function
+int main(void) { // main function
 
     int players;
     int seed;
@@ -93,7 +93,7 @@ int main (void){ // main function
     printf("How many players? ");
     scanf("%d", &players);
 
-    if ((players < 2) || (players > 10)){
+    if ((players < 2) || (players > 10)) {
         fprintf(stderr, "Invalid number of players. Using 2 instead. \n");
         players = 2;
     }
@@ -106,4 +106,4 @@ int main (void){ // main function
     game_master(players);
 
     return 0;
-}   
+}
