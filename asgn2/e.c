08692b819ae5b1b4b7ae74b666911a64f326bcc1
dtercia <dtercian@ucsc.edu>
2021-10-04 21:07:00 +0000

    adding e.c

---
+++ asgn2/e.c
@@ -0,0 +1,28 @@
+#include "mathlib.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static int terms;
+
+double e(void);
+int e_terms(void);
+
+double e(void) {
+    double factorial = 1, k = 1, sum = 0;
+    do {
+        for (double i = 1; i < k; ++i) {
+            factorial *= 1 / i;
+        }
+        k++;
+        sum += factorial;
+        terms++;
+    } while (factorial > EPSILON);
+
+    return sum;
+}
+
+int e_terms(void) {
+    return terms;
+}
