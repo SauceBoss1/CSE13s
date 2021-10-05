5476f760ae5f8359c1b246b514cc0464f2483eac
dtercia <dtercian@ucsc.edu>
2021-10-05 22:29:21 +0000

    added euler.c

--- asgn2/mathlib-test.c
+++ asgn2/mathlib-test.c
@@ -13,21 +13,12 @@ int main(int argc, char **argv) {
     int opt = 0;
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch (opt) {
-        case 'a': 
-	    a = e_num = b = m = r = v = n = s = h = true;
-	    break;
-        case 'e': 
-	    e_num = true; 
-	    break;
-        case 'n': 
-	    n = true; 
-	    break;
-	case 'm':
-	    m = true;
-	    break;
-        case 's': 
-	    s = true; 
-	    break;
+        case 'a': a = e_num = b = m = r = v = n = s = h = true; break;
+        case 'e': e_num = true; break;
+        case 'n': n = true; break;
+        case 'm': m = true; break;
+        case 's': s = true; break;
+        case 'r': r = true; break;
         }
     }
 
@@ -55,6 +46,14 @@ int main(int argc, char **argv) {
             absolute(pi_madhava() - M_PI));
         if (s) {
             printf("pi_madhava() terms = %d\n", pi_madhava_terms());
+        }
+    }
+
+    if (r) {
+        printf("pi_euler() = %16.15lf, M_PI = %16.15lf, diff = %16.15lf\n", pi_euler(), M_PI,
+            absolute(pi_euler() - M_PI));
+        if (s) {
+            printf("pi_euler() terms = %d\n", pi_euler_terms());
         }
     }
     return 0;
