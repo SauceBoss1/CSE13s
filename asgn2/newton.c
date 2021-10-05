b6015b48076d617647ae50f377df8e8bb808fcd0
dtercia <dtercian@ucsc.edu>
2021-10-05 18:35:40 +0000

    adding newton

---
+++ asgn2/newton.c
@@ -0,0 +1,21 @@
+#include "mathlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static int counter = 0;
+
+double sqrt_newton(double x){
+    double z = 0.0;
+    double y = 1.0;
+    while (absolute(y-z) > EPSILON){
+        counter++;
+        z=y;
+        y = 0.5 * (z + x/z);
+    }
+
+    return y;
+}
+
+int sqrt_newton_iters(void){
+    return counter;
+}
