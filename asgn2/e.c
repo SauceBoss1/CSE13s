61690c8013283fdd9f081b45787cebba49d6c7e8
dtercia <dtercian@ucsc.edu>
2021-10-05 19:57:49 +0000

    implemented test harness

--- asgn2/e.c
+++ asgn2/e.c
@@ -4,19 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int terms;
+static int terms = 0;
 
 double e(void) {
-    double factorial = 1, k = 1, sum = 0;
+    double factorial = 1, k = 1, sum = 0, term = 0;
     do {
+        factorial = 1;
         for (double i = 1; i < k; ++i) {
             factorial *= 1 / i;
         }
         k++;
         sum += factorial;
-        terms++;
+        term++;
     } while (factorial > EPSILON);
 
+    terms = term;
     return sum;
 }
 
--- asgn2/mathlib-test.c
+++ asgn2/mathlib-test.c
@@ -1,44 +1,43 @@
+#include "mathlib.h"
+
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <stdbool.h>
-#include "mathlib.h"
-#include <math.h>
 
 #define OPTIONS "aebmrvnsh"
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     bool a = false, e_num = false, b = false, m = false, r = false;
     bool v = false, n = false, s = false, h = false;
     int opt = 0;
-    while ((opt = getopt(argc, argv, OPTIONS)) != -1){
-        switch(opt){
-            case 'a':
-                a = e_num = b = m = r = v = n = s = h = true;
-                break;
-            case 'e':
-                e_num = true;
-                break;
-            case 'n':
-                n = true;
-        }       break;
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+        case 'a': a = e_num = b = m = r = v = n = s = h = true; break;
+        case 'e': e_num = true; break;
+        case 'n': n = true; break;
+        case 's': s = true; break;
+        }
     }
 
-    if (e_num){
+    if (e_num) {
         double diff = absolute(e() - M_E);
         printf("e() = %16.15lf, M_E = %16.15lf, diff = %16.15lf\n", e(), M_E, diff);
-        if (s){
-            printf("e() terms = %d",e_terms());
+        if (s) {
+            printf("e() terms = %d\n", e_terms());
         }
 
-    if (n){
-        for (double i = 0.0; i <= 10.0; i += 0.1){
-            double dif = absolute(sqrt_newton(i) - sqrt(i));
-            printf("sqrt_newton(%16.15lf) = %16.15lf sqrt(%16.15lf) = %16.15lf diff = %16.15lf", i, sqrt_newton(i), i, sqrt(i), diff);
-            if (s){
-                printf("sqrt_newton() terms = %d", sqrt_newton_iters());
+        if (n) {
+            for (double i = 0.0; i <= 10.0; i += 0.1) {
+                double diff = absolute(sqrt_newton(i) - sqrt(i));
+                printf(
+                    "sqrt_newton(%16.15lf) = %16.15lf sqrt(%16.15lf) = %16.15lf diff = %16.15lf\n",
+                    i, sqrt_newton(i), i, sqrt(i), diff);
+                if (s) {
+                    printf("sqrt_newton() terms = %d\n", sqrt_newton_iters());
+                }
             }
         }
-    }
     }
     return 0;
 }
