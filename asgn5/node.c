1a868bc6f43b083ab9ff2cff2db770d5ee633c9a
dtercia <dtercian@ucsc.edu>
2021-10-28 22:08:26 +0000

    adding nodes

---
+++ asgn5/node.c
@@ -0,0 +1,48 @@
+#include "node.h"
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+Node *node_create(uint8_t symbol, uint64_t frequency) {
+    Node *n = (Node *) malloc(sizeof(Node));
+    if (n) {
+        n->symbol = symbol;
+        n->frequency = frequency;
+    } else {
+        free(n);
+        n = NULL;
+    }
+
+    return n;
+}
+
+void node_delete(Node **n) {
+    if (*n) {
+        free(*n);
+        *n = NULL;
+    }
+    return;
+}
+
+Node *node_join(Node *left, Node *right) {
+    Node *n = node_create('$', left->frequency + right->frequency);
+    return n;
+}
+
+void node_print(Node *n) {
+    printf("symbol: %" PRIu8 " frequence: %" PRIu64 "\n", n->symbol, n->frequency);
+}
+
+//int main(void) {
+//    Node *left = node_create('a', 3);
+//    Node *right = node_create('a', 2);
+//    Node *n = node_join(left, right);
+//    node_print(n);
+
+//    node_delete(&n);
+//    node_delete(&left);
+//    node_delete(&right);
+//}
+
