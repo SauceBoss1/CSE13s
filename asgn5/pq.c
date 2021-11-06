aaf77cf77763a6ded6721d82ae0e85586b160cf8
dtercia <dtercian@ucsc.edu>
2021-11-06 03:24:07 +0000

    pq

--- asgn5/pq.c
+++ asgn5/pq.c
@@ -51,7 +51,7 @@ void pq_delete(PriorityQueue **q) {
 
 /////////////////////////////////////////////
 //HEAP IMPLEMENTATION BELOW
-
+/*
 static uint32_t min_child(PriorityQueue *q, uint32_t first, uint32_t last) {
     uint32_t left = 2 * first;
     uint32_t right = left + 1;
@@ -60,7 +60,7 @@ static uint32_t min_child(PriorityQueue *q, uint32_t first, uint32_t last) {
         return left;
     }
     return right;
-}
+}*/
 
 void swap(Node *x, Node *y) {
     //puts("here");
@@ -71,14 +71,12 @@ void swap(Node *x, Node *y) {
     return;
 }
 
+/*
 static void fix_heap(PriorityQueue *q, uint32_t first, uint32_t last) {
     bool found = false;
     uint32_t mother = first, great = min_child(q, mother, last);
 
     while ((mother <= floor(last / 2)) && !found) {
-        if (q->items[0]->frequency == 0) {
-            swap(q->items[0], q->items[q->top - 1]);
-        }
         if (q->items[mother - 1]->frequency > q->items[great - 1]->frequency) {
             swap(q->items[mother - 1], q->items[great - 1]);
             mother = great;
@@ -88,6 +86,38 @@ static void fix_heap(PriorityQueue *q, uint32_t first, uint32_t last) {
         }
     }
     return;
+}*/
+
+
+void min_insert_heap(PriorityQueue *q, uint32_t i){
+    //recommended by TA in section
+    uint32_t parent = (i - 1)/2;
+    while( i >= 0 && (q->items[parent]->frequency > q->items[i]->frequency) ){
+        swap(q->items[parent], q->items[i]);
+        i = parent;
+        parent = i / 2;
+    }
+}
+
+void fix_heap(PriorityQueue *q, uint32_t i){
+    uint32_t left = i * 2;
+    uint32_t right = i * 2 + 1;
+    uint32_t small = 0;
+    
+    if ((left <= pq_size(q)) && (q->items[left]->frequency < q->items[i]->frequency)){
+        small = left;
+    } else {
+        small = i;
+    }
+
+    if ((right <= pq_size(q)) && (q->items[right]->frequency < q->items[small]->frequency)){
+        small = right;
+    }
+
+    if (small != i){
+        swap(q->items[i], q->items[small]);
+        fix_heap(q, small);
+    }
 }
 
 ///////////////////////////////////////////
@@ -114,9 +144,14 @@ bool enqueue(PriorityQueue *q, Node *n) {
     if (pq_full(q)) {
         return false;
     }
-
+	if (pq_empty(q)){
+		q->items[q->top] = n;
+		q->top++;
+		return true;
+	}
     q->items[q->top] = n;
-    fix_heap(q, 1, q->top);
+    //fix_heap(q, 1, q->top);
+    min_insert_heap(q, q->top);
     q->top++;
 
     return true;
@@ -126,11 +161,15 @@ bool dequeue(PriorityQueue *q, Node **n) {
     if (pq_empty(q)) {
         return false;
     }
-
-    swap(q->items[0], q->items[q->top - 1]);
-    *n = q->items[q->top - 1];
-    q->top--;
-    fix_heap(q, 1, q->top);
+	*n = q->items[0];
+	q->items[0] = q->items[q->top - 1];
+	q->top--;
+	fix_heap(q,0);
+    //fix_heap(q, 1, q->top);
+    //swap(q->items[0], q->items[q->top - 1]);
+    //*n = q->items[q->top - 1];
+    //q->top--;
+    //fix_heap(q, 1, q->top);
 
     return true;
 }
@@ -138,7 +177,7 @@ bool dequeue(PriorityQueue *q, Node **n) {
 void pq_print(PriorityQueue *q) {
     printf("[");
     for (uint32_t i = 0; i < q->top; ++i) {
-        printf(" %" PRIu64, q->items[i]->frequency); //tries to print NULL leading to segfault
+        printf(" {%c, %"PRIu64"}", q->items[i]->symbol, q->items[i]->frequency); 
     }
     printf(" ]\n");
 
