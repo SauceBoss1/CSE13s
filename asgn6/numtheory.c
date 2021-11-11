e3611043149d9d5db1dd0180145b815aa7e69b1d
dtercia <dtercian@ucsc.edu>
2021-11-11 07:10:26 +0000

    num

--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -8,29 +8,31 @@
 #include <inttypes.h>
 
 void pow_mod(mpz_t o, mpz_t a, mpz_t d, mpz_t n) {
-    mpz_t v;
-    mpz_t p;
-    mpz_t temp;
-    mpz_inits(v, p, temp, NULL);
+    mpz_t v, p, temp, a_temp, d_temp, n_temp;
+    mpz_inits(v, p, temp, a_temp, d_temp, n_temp, NULL);
+
+    mpz_set(a_temp, a);
+    mpz_set(d_temp, d);
+    mpz_set(n_temp, n);
 
     mpz_set_ui(v, 1);
-    mpz_set(p, a);
-    while (mpz_cmp_ui(d, 0) > 0) {
-        if (mpz_odd_p(d) != 0) {
+    mpz_set(p, a_temp);
+    while (mpz_cmp_ui(d_temp, 0) > 0) {
+        if (mpz_odd_p(d_temp) != 0) {
             // v = (v*p) % n
             mpz_mul(temp, v, p);
-            mpz_mod(v, temp, n);
+            mpz_mod(v, temp, n_temp);
         }
         //p = (p*p) % n
         mpz_mul(temp, p, p);
-        mpz_mod(p, temp, n);
+        mpz_mod(p, temp, n_temp);
 
         //d = d/2
-        mpz_fdiv_q_ui(d, d, 2);
+        mpz_fdiv_q_ui(d_temp, d_temp, 2);
     }
 
     mpz_set(o, v);
-    mpz_clears(v, p, temp, NULL);
+    mpz_clears(v, p, temp, a_temp, d_temp, n_temp NULL);
     return;
 }
 
@@ -43,11 +45,15 @@ bool is_prime(mpz_t n, uint64_t iters) {
         return true;
     }
 
+    mpz_t n_temp;
+    mpz_init(n_temp);
+    mpz_set(n_temp, n);
+
     //find an r that is odd such that n = 2^s * r + 1
     mpz_t s;
     mpz_init(s);
     //s = n - 1
-    mpz_sub_ui(s, n, 1);
+    mpz_sub_ui(s, n_temp, 1);
 
     while (mpz_even_p(s) != 0) {
         //s /= 2
@@ -57,20 +63,19 @@ bool is_prime(mpz_t n, uint64_t iters) {
     mpz_t a, x, temp, y, n_minus_1, j, s_minus_1, two;
     mpz_inits(a, x, temp, y, n_minus_1, j, s_minus_1, two, NULL);
 
+    mpz_sub_ui(n_minus_1, n_temp, 1);
+    mpz_sub_ui(s_minus_1, s, 1);
+    mpz_set_ui(two, 2);
     //m-r primality testing algorithm
     for (uint64_t i = 1; i < iters; ++i) {
         //uint64_t a = (uint64_t) (rand() % ( n - 4 )) + 2;
-        mpz_urandomm(a, state, n);
-        mpz_sub_ui(temp, n, 3); //may need to change to 4
+        mpz_urandomm(a, state, n_temp);
+        mpz_sub_ui(temp, n_temp, 3); //may need to change to 4
         mpz_mod(a, a, temp);
         mpz_add_ui(a, a, 2);
 
         //uint64_t y = pow_mod(a, s, n);
-        pow_mod(y, a, s, n);
-
-        mpz_sub_ui(n_minus_1, n, 1);
-        mpz_sub_ui(s_minus_1, s, 1);
-        mpz_set_ui(two, 2);
+        pow_mod(y, a, s, n_temp);
 
         //if( (y != 1) && (y != n - 1))
         if ((mpz_cmp_ui(y, 1) != 0) && (mpz_cmp(y, n_minus_1) != 0)) {
@@ -79,12 +84,13 @@ bool is_prime(mpz_t n, uint64_t iters) {
             //while ((j <= s - 1) && y != n - 1)
             while ((mpz_cmp(j, s_minus_1) <= 0) && (mpz_cmp(y, n_minus_1) != 0)) {
                 //y = pow_mod(y, 2, n);
-                pow_mod(y, y, two, n);
+                pow_mod(y, y, two, n_temp);
 
                 //if (y == 1)
                 if (mpz_cmp_ui(y, 1) == 0) {
                     return false;
                 }
+
                 //j = j + 1;
                 mpz_add_ui(j, j, 1);
             }
@@ -95,17 +101,18 @@ bool is_prime(mpz_t n, uint64_t iters) {
             }
         }
     }
-    mpz_clears(s, a, x, temp, y, n_minus_1, j, s_minus_1, two, NULL);
+    mpz_clears(s, a, x, temp, y, n_minus_1, j, s_minus_1, two, n_temp, NULL);
     return true;
 }
 
 void make_prime(mpz_t p, uint64_t bits, uint64_t iters) {
     mpz_t temp;
     mpz_init(temp);
+    mp_bitcnt_t nbits = bits;
 
     do {
-        mpz_urandomb(temp, state, bits);
-    } while (is_prime(temp, iters));
+        mpz_urandomb(temp, state, nbits);
+    } while (!is_prime(temp, iters));
 
     mpz_set(p, temp);
     mpz_clear(temp);
@@ -132,12 +139,15 @@ void gcd(mpz_t g, mpz_t a, mpz_t b) {
 }
 
 void mod_inverse(mpz_t o, mpz_t a, mpz_t n) {
-    mpz_t r, r_prime, t, t_prime, q, temp, mul_temp;
-    mpz_inits(r, r_prime, t, t_prime, q, temp, mul_temp);
+    mpz_t r, r_prime, t, t_prime, q, temp, mul_temp, a_temp, n_temp;
+    mpz_inits(r, r_prime, t, t_prime, q, temp, mul_temp, a_temp, n_temp);
+
+    mpz_set(a_temp, a);
+    mpz_set(n_temp, n);
 
     //(r,r_prime)←(n,a)
-    mpz_set(r, n);
-    mpz_set(r_prime, a);
+    mpz_set(r, n_temp);
+    mpz_set(r_prime, a_temp);
 
     //(t,t_prime)←(0,1)
     mpz_set_ui(t, 0);
@@ -171,7 +181,7 @@ void mod_inverse(mpz_t o, mpz_t a, mpz_t n) {
     //if t < 0
     if (mpz_cmp_ui(temp, 0) < 0) {
         //t←t+n
-        mpz_add(t, t, n);
+        mpz_add(t, t, n_temp);
     }
 
     //return t
