0095932864748f415bba1a1dda89a83cdb5e501b
dtercia <dtercian@ucsc.edu>
2021-11-11 06:32:11 +0000

    num

--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -11,7 +11,7 @@ void pow_mod(mpz_t o, mpz_t a, mpz_t d, mpz_t n) {
     mpz_t v;
     mpz_t p;
     mpz_t temp;
-    mpz_inits(v, p, temp);
+    mpz_inits(v, p, temp, NULL);
 
     mpz_set_ui(v, 1);
     mpz_set(p, a);
@@ -55,7 +55,7 @@ bool is_prime(mpz_t n, uint64_t iters) {
     }
 
     mpz_t a, x, temp, y, n_minus_1, j, s_minus_1, two;
-    mpz_inits(a, x, temp, y, n_minus_1, j, s_minus_1, two);
+    mpz_inits(a, x, temp, y, n_minus_1, j, s_minus_1, two, NULL);
 
     //m-r primality testing algorithm
     for (uint64_t i = 1; i < iters; ++i) {
@@ -97,6 +97,85 @@ bool is_prime(mpz_t n, uint64_t iters) {
     }
     mpz_clears(s, a, x, temp, y, n_minus_1, j, s_minus_1, two);
     return true;
+}
+
+void make_prime(mpz_t p, uint64_t bits, uint64_t iters) {
+    mpz_t temp;
+    mpz_init(temp);
+
+    do {
+        mpz_urandomb(temp, state, bits);
+    } while (is_prime(temp, iters));
+
+    mpz_set(p, temp);
+    mpz_clear(temp);
+    return;
+}
+
+void gcd(mpz_t g, mpz_t a, mpz_t b) {
+    //uint64_t t = 0;
+    mpz_t t;
+    mpz_init(t);
+    mpz_set_ui(t, 0);
+
+    while (mpz_cmp_ui(b, 0) != 0) {
+        mpz_set(t, b);
+        mpz_mod(b, a, b);
+        mpz_set(a, t);
+    }
+
+    mpz_set(g, a);
+    return;
+}
+
+void mod_inverse(mpz_t o, mpz_t a, mpz_t n) {
+    mpz_t r, r_prime, t, t_prime, q, temp, mul_temp;
+    mpz_inits(r, r_prime, t, t_prime, q, temp, mul_temp);
+
+    //(r,r_prime)←(n,a)
+    mpz_set(r, n);
+    mpz_set(r_prime, a);
+
+    //(t,t_prime)←(0,1)
+    mpz_set_ui(t, 0);
+    mpz_set_ui(t_prime, 1);
+
+    //while r_prime != 0
+    while (mpz_cmp_ui(r_prime, 0) != 0) {
+        //q←r/r_prime
+        mpz_fdiv_q(q, r, r_prime);
+
+        //(r,r_prime)←(r_prime,r−q*r_prime)
+        mpz_set(temp, r);
+        mpz_set(r, r_prime);
+        mpz_mul(mul_temp, q, r_prime);
+        mpz_sub(r_prime, temp, mul_temp);
+
+        //(t,t_prime)←(t_prime,t−q*t_prime)
+        mpz_set(temp, t);
+        mpz_set(t, t_prime);
+        mpz_mul(mul_temp, q, t_prime);
+        mpz_sub(t_prime, temp, mul_temp);
+    }
+
+    //if r > 1
+    if (mpz_cmp_ui(r, 1) > 0) {
+        //return no inverse
+        mpz_set(o, 0);
+        return;
+    }
+
+    //if t < 0
+    if (mpz_cmp_ui(temp, 0) < 0) {
+        //t←t+n
+        mpz_add(t, t, n);
+    }
+
+    //return t
+    mpz_set(o, t);
+
+    mpz_clears(r, r_prime, t, t_prime, q, temp, mul_temp);
+    return;
 }
 
 int main(void) {
