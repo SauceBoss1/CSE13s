2211780d57cf4b50e968b153c368910230c61288
dtercia <dtercian@ucsc.edu>
2021-11-18 02:12:53 +0000

    comments

--- asgn6/randstate.c
+++ asgn6/randstate.c
@@ -7,12 +7,14 @@
 
 gmp_randstate_t state;
 
+//initalizes the state variable with a given seed
 void randstate_init(uint64_t seed) {
     gmp_randinit_mt(state);
     gmp_randseed_ui(state, seed);
     return;
 }
 
+//clears the state variable
 void randstate_clear(void) {
     gmp_randclear(state);
     return;
