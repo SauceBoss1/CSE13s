7443aa262f15b1f4e2f8b40df410635c212e6895
dtercia <dtercian@ucsc.edu>
2021-12-02 22:24:51 +0000

    plot

--- asgn7/plot.sh
+++ asgn7/plot.sh
@@ -2,29 +2,28 @@
 
 make
 
-for i in {0..10}; do
-    ./banhammer -s -a -t $(( 10**i )) -f $(( 10**i )) < ~/resources/corpora/calgary/news
+for i in {0..8}; do
+    ./banhammer -s -a -f $(( 10**i )) < ~/resources/corpora/calgary/news
 done
 
 gnuplot << EOF
 set terminal pdf
 set bmargin 4
 set key outside
-set size ration 0.75
-set xlabel "bf and ht size (they are set the same values)"
+set logscale x; set xtics 1, 100, 1e8
+set xlabel "bf size"
 
 set output "stats.pdf"
-set title "Statistics for avg_branches, avg_bst_height, and avg_bst_size"
+set title "Statistics for avg branches and avg bst height"
 set ylabel "Info"
-plot "./bst_size.dat" using 1:2 with linespoints title "avg_bst_size", \
-    "./bst_height.dat" using 1:2 with linespoints title "avg_bst_height", \
-    "./avg_branches.dat" using 1:2 with linespoints title " avg_branches traversed "
+plot "bst_height.dat" using 1:2 with linespoints title "avg bst height", \
+    "avg_branches.dat" using 1:2 with linespoints title " avg branches traversed "
 
 EOF
 
 rm -rf *.dat
 make clean
 
-git add *.jpg
-git commit -m "adding jpgs"
+git add *.pdf
+git commit -m "adding stats pdfs"
 git push
